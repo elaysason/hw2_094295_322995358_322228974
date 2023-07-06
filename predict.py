04fdce40388edae3d2dce1d996900ae9e8aa4cf3
Elay Sason <89357293+elaysason@users.noreply.github.com>
2023-07-06 19:17:55 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,12 +7,10 @@ from dataset import HW3Dataset
 class GAT(torch.nn.Module):
     def __init__(self, hidden_channels, heads):
         super().__init__()
-        torch.manual_seed(1234567)
         self.conv1 = GATConv(dataset.num_features,int(hidden_channels/2))
         self.conv2 = GATConv(int(hidden_channels/2),hidden_channels)
         self.conv3 = GATConv(hidden_channels,dataset.num_classes)
         # adding batch norm layers to generalize the data
-
         self.bn1 = torch.nn.BatchNorm1d(int(hidden_channels/2))
         self.bn2 = torch.nn.BatchNorm1d(int(hidden_channels))
 
@@ -33,16 +31,18 @@ class GAT(torch.nn.Module):
         return x
 
 
+
 if __name__ == '__main__':
     dataset = HW3Dataset(root='data/hw3/')
     data = dataset[0]
     with open('final_model.pkl', 'rb') as file:
         loaded_model = pickle.load(file)
     out = loaded_model(data.x, data.edge_index)
+    # getting the predictions
     predictions = out.argmax(dim=1)
-
+    # getting the index
     ids=[i for i in range(len(predictions))]
     data_dict = {'idx':ids,'prediction':predictions}
+    # saving and making a file for each index its prediction
     df_preidcted = pd.DataFrame(data=data_dict)
     df_preidcted.to_csv('prediction.csv',index=False)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
